0965256867ca241671b5db229f76de8b0992506b
AmmarSassi <sassi.ammar@hotmail.com>
2022-01-02 21:48:13 +0100

    Project initializing.

---
+++ src/main/java/com/example/JsonData/JsonDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.JsonData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JsonData/JsonDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JsonData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
